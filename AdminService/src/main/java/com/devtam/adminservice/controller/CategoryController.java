602aa5c665c92d9f9147f05c1cfb6fb0c8ff4a22
ddlthenk <boconganh2k2@gmail.com>
2024-11-17 14:09:38 +0700

    thêm admin controller

---
+++ AdminService/src/main/java/com/devtam/adminservice/controller/CategoryController.java
@@ -0,0 +1,26 @@
+package com.devtam.adminservice.controller;
+
+import com.devtam.commonbase.entity.Category;
+import com.devtam.commonbase.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+public class CategoryController {
+    @Autowired
+    CategoryService categoryService;
+
+    @GetMapping("/get-category-children/{parentId}")
+    @ResponseBody
+    public List<Category> getCategoryChildren(@PathVariable("parentId") long parentId) {
+        List<Category> categories = categoryService.getCategoriesByParentId(parentId);
+        return categories;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 Java Spring Boot application base on Microservice structure \
 File báo cáo: https://1drv.ms/w/s!Al0aIDbXYuPo7UFzeNm9DuWrV823?e=XqzG4N
+
+Chia project thành 3 module
+
+- CommonBase: module dùng chung
+- AdminService và UserService là xử lý nghiệp vụ riêng cho admin và user
+
+Các bước xây dựng dự án:
+
+- Khởi tạo dự án
+- tạo các entity(thực thể) theo bảng đã thiết kế
+- tạo môi trường, cài đặt mysql server
+- tạo repository layer: kết nối và tương tác với database
+- service là xử lý các logic nghiệp vụ
+- insertData là để fake data khi khởi chạy dự án
+- constant là các hằng số dùng chung
+- configuration là cấu hình cho dự án
+- common là các lớp xử lý logic dùng chung
+- util là các lớp công cụ
